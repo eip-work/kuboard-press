493e4272d7f60d4bd39514d9d53ee4de987d3ca5
huanqing.shao <shaohq@foxmail.com>
2019-07-25 06:22:52 +0800

    deploy

---
+++ .vuepress/dist/assets/js/36.8bff1b35.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[36],{9:function(n,w,o){}}]);
--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,6 @@ set -e
 npm run docs:build
 
 # navigate into the build output directory
-cd docs/.vuepress/dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
@@ -20,6 +19,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:eip-work/kuboard-press.git master
 
 cd -
