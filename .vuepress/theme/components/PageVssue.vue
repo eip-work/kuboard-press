a96e0b29779421f1d693e3a67e4473071f51f547
huanqing.shao <shaohq@foxmail.com>
2019-10-03 21:26:44 +0800

    去除360自动提交，以避免错误

--- .vuepress/theme/components/PageVssue.vue
+++ .vuepress/theme/components/PageVssue.vue
@@ -10,12 +10,6 @@
         <Vssue ref="vssue" :title="$page.path" :options="options"/>
       </div>
     </div>
-<script>
-(function(){
-var src = "https://jspassport.ssl.qhimg.com/11.0.1.js?d182b3f28525f2db83acfaaf6e696dba";
-document.write('<script src="' + src + '" id="sozz"><\/script>');
-})();
-</script>
   </div>
 </template>
 
