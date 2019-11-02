46ed041db23e66f87c51355aedaf2bb56dbdf121
huanqing.shao <shaohq@foxmail.com>
2019-11-02 10:53:51 +0800

    bootstrap

---
+++ .vuepress/theme/components/NavLink.vue
@@ -0,0 +1,60 @@
+<template>
+  <router-link
+    class="nav-link my-nav-link"
+    :to="link"
+    @focusout.native="focusoutAction"
+    v-if="!isExternal(link)"
+    :exact="exact"
+  >{{ item.text }}</router-link>
+  <a
+    v-else
+    :href="link"
+    @focusout="focusoutAction"
+    class="nav-link my-nav-link external"
+    :target="isMailto(link) || isTel(link) ? null : '_blank'"
+    :rel="isMailto(link) || isTel(link) ? null : 'noopener noreferrer'"
+  >
+    {{ item.text }}
+    <OutboundLink/>
+  </a>
+</template>
+
+<script>
+import { isExternal, isMailto, isTel, ensureExt } from '../util'
+
+export default {
+  props: {
+    item: {
+      required: true
+    }
+  },
+
+  computed: {
+    link () {
+      return ensureExt(this.item.link)
+    },
+
+    exact () {
+      if (this.$site.locales) {
+        return Object.keys(this.$site.locales).some(rootLink => rootLink === this.link)
+      }
+      return this.link === '/'
+    }
+  },
+
+  methods: {
+    isExternal,
+    isMailto,
+    isTel,
+    focusoutAction () {
+      this.$emit('focusout')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.my-nav-link {
+  padding: 0px;
+}
+</style>
