ef3b91c5ed2f832c2f9c89be8aa044ff5d083c3b
Shao Huan Qing <shaohq@foxmail.com>
2021-06-02 21:33:36 +0800

    window

--- .vuepress/login-manager.js
+++ .vuepress/login-manager.js
@@ -6,25 +6,29 @@ let TOKEN_KEY = 'kb-user-center-token'
 let domain = ''
 if (typeof window !== 'undefined') {
   domain = window.location.hostname
+  window.KbUcloginStatus = {
+    status: Cookies.get(TOKEN_KEY) !== null && Cookies.get(TOKEN_KEY) !== undefined,
+    user: undefined
+  }
 }
 if (domain.indexOf('kuboard.cn') >= 0) {
   domain = 'kuboard.cn'
 }
 
-window.KbUcloginStatus = {
-  status: Cookies.get(TOKEN_KEY) !== null && Cookies.get(TOKEN_KEY) !== undefined,
-  user: undefined
-}
 
 Vue.prototype.$login = function (token) {
   // axios.defaults.headers.common['Authorization'] = token;
   // localStorage.setItem(TOKEN_KEY, token)
   Cookies.set(TOKEN_KEY, token, { domain: domain, expires: 7, path: '/' })
-  window.KbUcloginStatus.status = true
+  if (typeof window !== 'undefined') {
+    window.KbUcloginStatus.status = true
+  }
 }
 
 Vue.prototype.$logout = function () {
-  window.KbUcloginStatus.status = false
+  if (typeof window !== 'undefined') {
+    window.KbUcloginStatus.status = false
+  }
   // localStorage.removeItem(TOKEN_KEY)
   Cookies.remove(TOKEN_KEY, { domain: domain, expires: 7, path: '/' })
   // delete (axios.defaults.headers.common['Authorization'])
