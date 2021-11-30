abaf94d5875d4b57d718e90ed038ad517afe71c1
Shao Huan Qing <shaohq@foxmail.com>
2021-11-30 20:30:14 +0800

    ad

--- .vuepress/components/ad-list.js
+++ .vuepress/components/ad-list.js
@@ -15,69 +15,28 @@ module.exports = [
     url: 'https://training.linuxfoundation.cn/certificates/1',
     weight: 100,
   },
-  {
-    name: '视频教程',
-    description: 'Kubernetes入门学习到企业实战的方方面面',
-    strong: 'Kuboard',
-    action: '免费试听',
-    url: 'https://ke.qq.com/course/package/32328?flowToken=1031971',
-    weight: 100
-  },
   // {
-  //   name: 'SOCK BOOM',
-  //   description: '我在这里 Fan Qiang 查阅 Kubernetes 第一手资料',
-  //   strong: '推荐',
-  //   action: '试一试',
-  //   url: 'https://sockboom.tel/auth/register?affid=182351',
+  //   name: '视频教程',
+  //   description: 'Kubernetes入门学习到企业实战的方方面面',
+  //   strong: 'Kuboard',
+  //   action: '免费试听',
+  //   url: 'https://ke.qq.com/course/package/32328?flowToken=1031971',
   //   weight: 100
   // },
-  {
-    name: '华为云',
-    description: '828企业上云节，亿元上云补贴',
-    strong: '限时抢购',
-    action: '立享优惠',
-    url: 'https://activity.huaweicloud.com/828_promotion/index.html?fromacct=36cf686d-2650-4107-baa4-f0dc3c860df4&utm_source=V1g3MDY4NTY=&utm_medium=cps&utm_campaign=201905',
-    weight: 60
-  },
+  // {
+  //   name: '华为云',
+  //   description: '828企业上云节，亿元上云补贴',
+  //   strong: '限时抢购',
+  //   action: '立享优惠',
+  //   url: 'https://activity.huaweicloud.com/828_promotion/index.html?fromacct=36cf686d-2650-4107-baa4-f0dc3c860df4&utm_source=V1g3MDY4NTY=&utm_medium=cps&utm_campaign=201905',
+  //   weight: 60
+  // },
   {
     name: '腾讯云',
     description: '【腾讯云】云产品限时秒杀。',
-    strong: '1核2G云服务器，首年99元',
+    strong: '1核2G云服务器，首年50元',
     action: '立即抢购',
     url: 'https://cloud.tencent.com/act/cps/redirect?redirect=1062&cps_key=2ee6baa049659f4713ddc55a51314372&from=console',
-    weight: 100
+    weight: 300
   },
-  // {
-  //   name: '阿里云',
-  //   description: '双十二，主会场，',
-  //   strong: '低至一折',
-  //   action: '去抢',
-  //   url: 'https://www.aliyun.com/1212/2019/home?userCode=obezo3pg',
-  //   weight: 30
-  // },
-  // {
-  //   name: 'Kubetrain',
-  //   description: 'K8S在线直播培训，内推机会',
-  //   strong: '不满意可无条件退款',
-  //   action: '现在就去',
-  //   url: 'https://kubetrain.cn/?from=kuboard-ads',
-  //   weight: 60
-  // },
-  // {
-  //   name: '12元直播课',
-  //   description: 'Kubernetes集群安装详解，安装脚本定制',
-  //   strong: '入门起点',
-  //   action: '捧个场',
-  //   url: 'https://ke.qq.com/course/477593?flowToken=1016937',
-  //   weight: 60
-  // }
-
-  // {
-  //   name: '宝塔面板',
-  //   description: '一键全能 Linux 部署及管理，',
-  //   strong: '送你3188元礼包',
-  //   action: '点我领取',
-  //   url: 'https://www.bt.cn/?invite_code=MV9vdnlveno=',
-  //   weight: 50
-  // }
 ]
