36004f09fa3527a5e1020965146ec4d0be8db3f2
shaohuanqing <shaohq@foxmail.com>
2026-08-03 22:00:34 +0800

    v4.2.0.0

--- build.sh
+++ build.sh
@@ -6,9 +6,11 @@ tag=swr.cn-east-2.myhuaweicloud.com/kuboard/kuboard-press
 pnpm install
 pnpm docs:build
 
-docker buildx build --platform linux/amd64 -t $tag:$datetime .
+docker buildx build --platform linux/amd64 -t $tag:$datetime . --load
 
 
 # echo "在宿主机上使用如下指令推送镜像到仓库"
 # echo "skopeo copy --dest-creds cn-east-2@\$SWR_AK:\$SWR_PW docker-daemon:$tag:$datetime docker://$tag:$datetime"
 docker run --rm -v /var/run/docker.sock:/var/run/docker.sock quay.io/skopeo/stable:v1.22 copy --dest-creds cn-east-2@$SWR_AK:$SWR_PW docker-daemon:$tag:$datetime docker://$tag:$datetime
+
+echo $tag:$datetime
